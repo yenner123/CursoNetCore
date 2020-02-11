29f5aa20870805f63309de02c343f3dd160142b3
yenner123 <yenner-159@hotmail.com>
2020-02-11 11:11:36 -0500

    Manejo de middleware en netcore

---
+++ EmployeeManagement/Startup.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // app.UseRouting();
+
+            app.UseStaticFiles();
+
+
+            app.Use(async (context, next) =>
+            {
+                //await context.Response.WriteAsync("Primer middleware");
+
+                logger.LogInformation("MW1: Incoming Request");
+                await next();
+                logger.LogInformation("MW1: Outgoing Request");
+            });
+
+
+            app.Use(async (context, next) =>
+            {
+                //await context.Response.WriteAsync("Primer middleware");
+
+                logger.LogInformation("MW2: Incoming Request");
+                await next();
+                logger.LogInformation("MW2: Outgoing Request");
+            });
+
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("MW3: middleware");
+                logger.LogInformation("MW3: Request handle and respose produced");
+            });
+        }
+    }
+}
